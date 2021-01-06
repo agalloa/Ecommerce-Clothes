1be8bb0f9ed69be490a5bd2eb369e482be3370d1
agalloa <30468747+agalloa@users.noreply.github.com>
2021-01-06 11:02:05 -0500

    beta01

---
+++ js/index.js
@@ -0,0 +1,23 @@
+/*Show menu */
+const showMenu = (toggleId, navId)=>{
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show-menu')
+        })
+    }
+}
+showMenu('nav-toggle', 'nav-menu')
+
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction(){
+    navLink.forEach(n => n.classList.remove('active'))
+    this.classList.add('active')
+
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
